c2588724c4147562c3ee6f060b4de99935a30976
Viktor Mörsell <viktor.morsell@northvolt.com>
2021-05-24 01:11:08 +0200

    Add Image component

---
+++ app/src/components/image.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import './image.css';
+
+export type ImageProps = {
+  imageRef: firebase.firestore.DocumentReference<firebase.firestore.DocumentData>;
+};
+
+export const Image = ({ imageRef }: ImageProps) => {
+  const [data, setData] = useState<firebase.firestore.DocumentData>();
+  const [error, setError] = useState(false);
+
+  const unsubscribe = imageRef.onSnapshot(
+    (s) => {
+      setData(s.data());
+    },
+    (error) => setError(true)
+  );
+
+  useEffect(() => {
+    if (data && data.storageURL) {
+      unsubscribe();
+    }
+  }, [data, unsubscribe]);
+
+  return (
+    <div
+      className="image"
+      style={{
+        backgroundImage:
+          !error && data && data.storageURL
+            ? `url(${data.storageURL})`
+            : 'none',
+      }}
+    >
+      {error && 'error'}
+    </div>
+  );
+};
