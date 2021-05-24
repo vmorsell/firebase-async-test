14ef09f621cc874f6039e7e7e418c76048833493
Viktor Mörsell <viktor.morsell@northvolt.com>
2021-05-24 02:27:51 +0200

    Spin logotype when adding images

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import logo from './logo.svg';
 import { Image } from './components/image';
 import './App.css';
@@ -17,10 +17,11 @@ function App() {
     firebase.firestore.DocumentReference<firebase.firestore.DocumentData>[]
   >([]);
   const [urlValue, setUrlValue] = useState('');
+  const logoRef = useRef<HTMLImageElement>(null);
 
   const handleAddURLClick = async () => {
-    console.log('adding image');
-    await addImage(urlValue);
+    spinLogo();
+    addImage(urlValue);
     setUrlValue('');
   };
 
@@ -35,6 +36,20 @@ function App() {
     setImageRefs([...imageRefs, res]);
   };
 
+  const spinLogo = async () => {
+    console.log('spin spin');
+    if (logoRef === null || logoRef.current === null) {
+      return;
+    }
+    const parent = logoRef.current.parentElement;
+    if (!parent) {
+      return;
+    }
+    parent.removeChild(logoRef.current);
+    parent.appendChild(logoRef.current);
+    logoRef.current.classList.add('spin');
+  };
+
   useEffect(() => {
     if (imageRefs.length > 4) {
       setImageRefs(imageRefs.slice(1));
@@ -44,7 +59,7 @@ function App() {
   return (
     <div className="app">
       <header className="header">
-        <img src={logo} className="logo" alt="logo" />
+        <img ref={logoRef} src={logo} className="logo" alt="logo" />
       </header>
       <main className="main">
         <div className="form">
